8a3f542c27603f82f1f9185cb545b55f9280e9d3
Selamawit926 <se.selamawit.elias@gmail.com>
2021-02-16 22:09:30 +0300

    Gallery has been added using carousel

---
+++ Lab_Exercise_07/AsyncDemo/assets/js/gallery.js
@@ -0,0 +1,54 @@
+const display= document.getElementById("carousel");
+const slide=document.getElementById("slide");
+
+document.addEventListener("DOMContentLoaded",displaySlide);
+
+
+function displaySlide(){
+
+    fetch('https://picsum.photos/v2/list')
+    .then(function(res) {
+        return res.json(); //return the JSON Promise
+    })
+    .then(function(posts) {
+        //iterate over each post [100 posts]
+        let output = '';
+       
+        posts.forEach(function(post) {
+
+            if (post==posts[0]){
+                output+= `
+    
+                <div class="carousel-item active">
+                
+                    <img src="${post.download_url}" class="img">
+            </div>
+        
+        `;
+       
+        }
+            else{
+                output += `
+        
+                <div class="carousel-item">
+                
+                    <img src="${post.download_url}" class="img">
+            
+            </div>
+        
+        `;
+        // console.log("I'm working");
+        }
+
+
+        });
+        display.innerHTML = output;
+    
+        // console.log("I work");
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+
+}
